5688e9b2990d6f87a391566877f2e92330d8d141
Aniruddh Gupta <aniruddh.gupta@nutanix.com>
2022-10-17 22:53:59 +0530

    pagination-advanced

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -19,6 +19,9 @@ export const PaginationTable = () => {
     canNextPage, // to check if we can move to next page or it exists or not,
     pageOptions, // to get the total number of page
     state, // current state of the table
+    gotoPage, // to navigate directly to a particular page
+    pageCount, // to get total page count
+    setPageSize, // set the size of pagination
     prepareRow
   } = useTable({
     columns,
@@ -27,7 +30,7 @@ export const PaginationTable = () => {
   usePagination); // pass usePagination to useTable
 
   // to get the current page index
-  const { pageIndex } = state;
+  const { pageIndex, pageSize } = state;
 
   return (
     <>
@@ -54,18 +57,47 @@ export const PaginationTable = () => {
           })}
         </tbody>
       </table>
-      <span>
+      <div>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
           Page{' '}
           <strong>
             {pageIndex + 1} of {pageOptions.length}
           </strong>{' '}
         </span>
-      <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-        Previous Page
-      </button>
-      <button onClick={() => nextPage()} disabled={!canNextPage}>
-        Next Page
-      </button>
+        <span>
+          | Go to page:{' '}
+          <input
+            type='number'
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(pageNumber)
+            }}
+            style={{ width: '50px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => setPageSize(Number(e.target.value))}>
+          {[10, 25, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
     </>
   )
 }
